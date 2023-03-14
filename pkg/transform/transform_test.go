a893e5166dc7f44aa6f7f9b047f1616e11aca18a
Gert Drapers <gert.drapers@live.com>
2023-03-14 08:32:11 -0700

    rebase

--- pkg/transform/transform_test.go
+++ pkg/transform/transform_test.go
@@ -1,10 +1,11 @@
-package transform
+package transform_test
 
 import (
 	"reflect"
 	"testing"
 
 	azureADTestUtils "github.com/aserto-dev/aserto-idp-plugin-azuread/pkg/testutils"
+	"github.com/aserto-dev/aserto-idp-plugin-azuread/pkg/transform"
 	"github.com/microsoftgraph/msgraph-sdk-go/models"
 	"github.com/stretchr/testify/require"
 )
@@ -13,7 +14,7 @@ func TestTransformToAzureAD(t *testing.T) {
 	assert := require.New(t)
 	apiUser := azureADTestUtils.CreateTestAPIUser("1", "Name", "email", "pic")
 
-	azureadUser := ToAzureAD(apiUser)
+	azureadUser := transform.ToAzureAD(apiUser)
 
 	assert.True(reflect.TypeOf(azureadUser) == reflect.TypeOf(models.NewUser()), "the returned object should be *models.User")
 	assert.Equal("Name", *(*azureadUser).GetDisplayName(), "should correctly detect the display name")
@@ -24,7 +25,7 @@ func TestTransform(t *testing.T) {
 	assert := require.New(t)
 	azureadUser := azureADTestUtils.CreateTestAzureADUser("1", "Name", "email", "pic", "+40722332233", "userName")
 
-	apiUser := *Transform(azureadUser)
+	apiUser := *transform.Transform(azureadUser)
 
 	assert.Equal("1", apiUser.Id, "should correctly populate the id")
 	assert.Equal("Name", apiUser.DisplayName, "should correctly detect the displayname")
